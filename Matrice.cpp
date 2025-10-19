6bcddcda593c6e46be572b65015ecc4057885361
AYAACHRAF20 <ayaachraf20@gmail.com>
2025-10-19 18:21:07 +0100

    Update Matrice.cpp

--- Matrice.cpp
+++ Matrice.cpp
@@ -20,18 +20,17 @@ Matrice::Matrice()
 
 Matrice::Matrice(const Matrice& m)
 {
-    this->ligne = m.ligne;
-    this->colonne = m.colonne;
-	
-    this->data = new int*[ligne];
-    for(int i = 0; i < this->ligne; i++)
-    {
-        this->data[i] = new int[colonne];
-        for(int j = 0; j < this->colonne; j++)
-        {
-            this->data[i][j] = m.data[i][j]; 
-        }
-    }
+	this->ligne = m.ligne;
+	this->colonne = m.colonne;
+	this->data = new int* [this->ligne];
+	for (int i = 0; i < this->ligne; i++) {
+		this->data[i] = new int[this->colonne];
+	}
+	for (int i = 0; i < this->ligne; i++) {
+		for (int j = 0; j < this->colonne; j++) {
+			this->data[i][j] = m.data[i][j];
+		}
+	}
 }
 
 void Matrice::afficher() const
